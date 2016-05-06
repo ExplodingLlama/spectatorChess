8d83c8a8ead1a1431ed4434d2f76f1cec5a66b59
Ratan <ratan@Ratans-MacBook-Pro.local>
2016-05-06 21:59:25 +0530

    a little change again

--- spectatorchess.js
+++ spectatorchess.js
@@ -1,11 +1,13 @@
 var http = require("http");
 //var chessBoard = require("chessboardjs-0.3.0");
 
-http.createServer(function (request, response) {
+var server = http.createServer(function (request, response) {
 	
 	response.writeHead(200, {'Content-Type': 'text/plain'});
 	response.end('Hello World\n');
-}).listen(8484);
+});
 
-console.log('Server is running on 8484 port');
+var port_number = server.listen(process.env.PORT || 8484);
+
+console.log('Server is running on ' + port_number.toString() + ' port');
 
